edf86833d0d7ef54d8d3cfb3456a14ab951ffb45
Anthony Rodriguez <anthony@nezia.dev>
2022-10-04 15:51:48 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@ then
 	exit
 fi
 
-# OS specific config
 os_name=$(head -1 /etc/os-release | cut -d "=" -f 2 | tr -d '"')
 
+# OS specific config
 case $os_name in
 	"Ubuntu")
 		cat ppa_list | xargs add-apt-repository -y
@@ -15,46 +15,49 @@ case $os_name in
 		;;
 esac
 
+# wsl specific config
 if [ -n "$(uname -a | grep -i "microsoft")" ] 
 then
-	if [ ! -f /etc/wsl.conf ]
+	# im sorry
+	username=$(/mnt/c/Windows/System32/cmd.exe /c "echo %USERNAME%" 2> /dev/null | tr -d '\r' )
+
+	# https://x410.dev/cookbook/wsl/sharing-windows-fonts-with-wsl/
+	if [ ! -f /etc/fonts/local.conf ]
 	then
-		cp wsl.conf /etc/wsl.conf
 		echo "integrating windows fonts inside wsl..."
+		cat << EOF | tee -a /etc/fonts/local.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+    <dir>/mnt/c/Windows/Fonts</dir>
+    <dir>/mnt/c/Users/$username/AppData/Local/Microsoft/Windows/Fonts</dir>
+</fontconfig>
+EOF
 		fc-cache && echo "done!"
 	fi
 
-	# im sorry
-	# watch out for windows commands in wsl (make sure to translate windows carriage returns to linux otherwise you're going to debug it for hours and lose your mind
-
+	# https://superuser.com/questions/1183176/can-i-share-my-ssh-keys-between-wsl-and-windows
 	if [ ! -d "/home/$SUDO_USER/.ssh" ]
 	then
-		# https://superuser.com/questions/1183176/can-i-share-my-ssh-keys-between-wsl-and-windows
 		echo "symlinking windows ssh keys with wsl..."
-		username=$(/mnt/c/Windows/System32/cmd.exe /c "echo %USERNAME%" 2> /dev/null | tr -d '\r' )
-		# Create .ssh directory in home directory with proper permission
+		# create .ssh directory in home directory with proper permissions
 		mkdir -m 700 "/home/$SUDO_USER/.ssh"
-		
-		# Add a permanent mount entry for Windows user .ssh directory
-		cat << EOF | sudo tee -a /etc/fstab
+		# add a permanent mount entry for Windows user .ssh directory
+		[-n $(grep "ssh" /etc/fstab) ] && cat << EOF | sudo tee -a /etc/fstab
 		C:\Users\\$username\.ssh\ /home/$SUDO_USER/.ssh drvfs rw,noatime,uid=1000,gid=1000,case=off,umask=0077,fmask=0177 0 0
 EOF
-		# Mount the .ssh
+		# mount .ssh
 		mount /home/$SUDO_USER/.ssh && echo "done!"
 	fi	
 fi
 
-# symlink all dotfile directories
+# symlink all packages
 find . -maxdepth 1 -type d -not -path '*/.*' -not -path '.' -execdir basename {} \; | xargs -I {} stow -Sv {}
 
 # fish setup
 # change shell to fish
 chsh -s /usr/bin/fish
 
-# powerline install
-pip install powerline-status
-
 # fisher install
 fish bootstrap_fish.fish
-
 
