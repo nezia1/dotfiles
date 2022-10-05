9a2668949e9b91d71fc4fe9dd7799af3a3f70dcb
Anthony Rodriguez <anthony@nezia.xyz>
2022-10-05 15:59:27 +0200

    update fish config to work with disabled wslg

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,7 @@ eval (keychain --eval --agents ssh -Q --quiet ssh id_ed25519 --nogui)
 set wsl_version_name (uname -r | sed -E 's/^.*(Microsoft|microsoft-standard).*$/\1/')
 if test -n $wsl_version_name
 	# if wslg is not present, set the DISPLAY variable to work with external X servers
-	if test ! -d "/mnt/wslg"
+	if test -z (ls -A /mnt/wslg)
 		set -Ux DISPLAY (grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0
 	end
 end
