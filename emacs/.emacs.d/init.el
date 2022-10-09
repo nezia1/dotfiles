6e713ec1d65e2005314285a7e04a25c226038b69
Anthony Rodriguez <anthony@nezia.xyz>
2022-10-09 23:04:33 +0200

    update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,6 +5,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons yasnippet-snippets lsp-ui general vue-mode company lsp-ivy web-mode which-key flycheck fira-code-mode ivy magit projectile dired-sidebar vscode-icon evil-collection evil doom-modeline nord-theme no-littering use-package))
  '(warning-suppress-log-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
