591c353f11b31b942be02161536545049c47c996
Anthony Rodriguez <anthony@nezia.xyz>
2022-10-17 12:09:49 +0200

    fix ivy

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,13 +1,11 @@
 (org-babel-load-file "~/.emacs.d/config.org")
- 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons yasnippet-snippets lsp-ui general vue-mode company lsp-ivy web-mode which-key flycheck fira-code-mode ivy magit projectile dired-sidebar vscode-icon evil-collection evil doom-modeline nord-theme no-littering use-package))
- '(warning-suppress-log-types '((lsp-mode))))
+   '(general vue-mode yasnippet-snippets company lsp-ivy lsp-ui web-mode which-key flycheck fira-code-mode ivy magit projectile dired-sidebar vscode-icon evil-collection evil doom-modeline all-the-icons nord-theme no-littering use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
