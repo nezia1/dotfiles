b490e3b448699228162e48d3c4c417c56ec3e9a6
Anthony Rodriguez <anthony@nezia.xyz>
2022-10-17 12:16:46 +0200

    add org-mode-bullets

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,8 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ispell-dictionary nil)
  '(package-selected-packages
-   '(general vue-mode yasnippet-snippets company lsp-ivy lsp-ui web-mode which-key flycheck fira-code-mode ivy magit projectile dired-sidebar vscode-icon evil-collection evil doom-modeline all-the-icons nord-theme no-littering use-package)))
+   '(org-bullets general vue-mode yasnippet-snippets company lsp-ivy lsp-ui web-mode which-key flycheck fira-code-mode ivy magit projectile dired-sidebar vscode-icon evil-collection evil doom-modeline all-the-icons nord-theme no-littering use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
