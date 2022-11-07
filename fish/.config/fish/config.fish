d0740af44d6020ead10101b8db775d02975fe9ba
Anthony Rodriguez <anthony@nezia.dev>
2022-11-07 15:23:29 +0100

    add emacs alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 # setup keychain
-eval (keychain --eval --agents ssh -Q --quiet ssh id_ed25519 --nogui)
+# eval (keychain --eval --agents ssh -Q --quiet ssh id_ed25519 --nogui)
 
 # wsl related config
 set wsl_version_name (uname -r | sed -E 's/^.*(Microsoft|microsoft-standard).*$/\1/')
@@ -10,4 +10,9 @@ if test -n $wsl_version_name
 		set -Ux DISPLAY (grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):0
 		set -Ux LIBGL_ALWAYS_REDIRECT 1
 	end
+end
+
+# Aliases
+if [ -f $HOME/.config/fish/alias.fish ]
+source $HOME/.config/fish/alias.fish
 end
