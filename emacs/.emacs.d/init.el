bbad1236318eb97583fe845cb23c2f5f8edae625
Anthony Rodriguez <anthony@nezia.dev>
2023-06-02 08:54:25 +0200

    increase emacs font size

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -29,7 +29,7 @@
 					;disable auto save
 (setq auto-save-default nil)
 
-(add-to-list 'default-frame-alist '(font . "Monospace 13"))
+(add-to-list 'default-frame-alist '(font . "Monospace 14"))
 ;; Set theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'catppuccin t)
