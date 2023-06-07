aa4727aae98655662a2b1f320f6641feb2ffd3f7
Anthony Rodriguez <anthony@nezia.dev>
2023-06-07 16:36:23 +0200

    make font a bit bigger

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,7 +14,7 @@
 					;disable auto save
 (setq auto-save-default nil)
 
-(add-to-list 'default-frame-alist '(font . "Monospace 14"))
+(add-to-list 'default-frame-alist '(font . "Monospace 15"))
 (add-hook 'text-mode-hook 'visual-line-mode)
 
 ;; Initialize package sources
@@ -178,16 +178,3 @@
 
 (use-package company-box
   :hook (company-mode . company-box-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(lsp-ui yasnippet which-key vertico use-package rustic pdf-tools org-roam magit lsp-mode guess-language expand-region exec-path-from-shell doom-modeline company-box catppuccin-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
