37a012479c7571ce05c8f6d8a6d8c4789d496613
Anthony Rodriguez <anthony@nezia.dev>
2024-06-25 12:29:32 +0200

    Add .emacs.d/config.org Add .emacs.d/init.el

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,18 @@
+(package-initialize)
+
+(org-babel-load-file "~/.emacs.d/config.org")
+
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(typescript-mode rust-mode which-key org-superstar org-superstar-mode vertico doom-modeline catppuccin-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
