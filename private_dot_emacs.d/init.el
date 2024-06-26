266ef98e72e52bda97cce628afaa514b0b01c49e
Anthony Rodriguez <anthony@nezia.dev>
2024-06-26 23:57:47 +0200

    Update .emacs.d/init.el

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(typescript-mode rust-mode which-key org-superstar org-superstar-mode vertico doom-modeline catppuccin-theme)))
+   '(rust-mode which-key org-superstar org-superstar-mode vertico doom-modeline catppuccin-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
