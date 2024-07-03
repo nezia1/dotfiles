9e701b8025b60b08de6eb5ed63e5c8f0ce8475d0
Anthony Rodriguez <anthony@nezia.dev>
2024-07-03 16:01:51 +0200

    Update .emacs.d/config.org Update .emacs.d/init.el

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -8,8 +8,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(rust-mode which-key org-superstar org-superstar-mode vertico doom-modeline catppuccin-theme)))
+ '(package-selected-packages '(eglot-booster))
+ '(package-vc-selected-packages
+   '((eglot-booster :vc-backend Git :url "https://github.com/jdtsmith/eglot-booster")
+     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
