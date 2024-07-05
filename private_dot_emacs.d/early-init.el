5372349f6e92b249b1e513fa17145d2191faa95a
Anthony Rodriguez <anthony@nezia.dev>
2024-07-05 02:57:55 +0200

    Add .emacs.d/README.org Add .emacs.d/early-init.el

---
+++ private_dot_emacs.d/early-init.el
@@ -0,0 +1,16 @@
+;; Paths to config.org and the tangled init.el
+(defvar org-config-file (expand-file-name "README.org" user-emacs-directory))
+(defvar tangled-init-file (expand-file-name "init.el" user-emacs-directory))
+
+;; Function to tangle the org file
+(defun tangle-org-config ()
+  (require 'org)
+  (org-babel-tangle-file org-config-file)
+  (load tangled-init-file))
+
+;; Check if config.org is newer than init.el and tangle if necessary
+(when (file-newer-than-file-p org-config-file tangled-init-file)
+  (tangle-org-config))
+
+
+
