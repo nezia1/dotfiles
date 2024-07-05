e07753d8fa1716a43a0829a968c5314f83b706a1
Anthony Rodriguez <anthony@nezia.dev>
2024-07-05 03:03:21 +0200

    Update .emacs.d/early-init.el

--- private_dot_emacs.d/early-init.el
+++ private_dot_emacs.d/early-init.el
@@ -5,8 +5,7 @@
 ;; Function to tangle the org file
 (defun tangle-org-config ()
   (require 'org)
-  (org-babel-tangle-file org-config-file)
-  (load tangled-init-file))
+  (org-babel-tangle-file org-config-file))
 
 ;; Check if config.org is newer than init.el and tangle if necessary
 (when (file-newer-than-file-p org-config-file tangled-init-file)
