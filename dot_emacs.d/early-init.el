2f7cfcbe2039b7f351c12e7c44bcf6324fcaafdb
Anthony Rodriguez <anthony@nezia.dev>
2024-07-16 23:42:02 +0200

    Update .emacs.d/README.org Update .emacs.d/early-init.el

--- dot_emacs.d/early-init.el
+++ dot_emacs.d/early-init.el
@@ -1,3 +1,6 @@
+;; Enable local variables
+(setq enable-local-variables :safe)
+
 ;; Paths to config.org and the tangled init.el
 (defvar org-config-file (expand-file-name "README.org" user-emacs-directory))
 (defvar tangled-init-file (expand-file-name "init.el" user-emacs-directory))
@@ -10,6 +13,4 @@
 ;; Check if config.org is newer than init.el and tangle if necessary
 (when (file-newer-than-file-p org-config-file tangled-init-file)
   (tangle-org-config))
-
-
 
