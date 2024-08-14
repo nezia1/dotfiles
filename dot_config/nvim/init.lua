248ba1931b19fb2a848e58ea2157700e06329be2
Anthony Rodriguez <anthony@nezia.dev>
2024-08-14 20:22:25 +0200

    Update .chezmoidata/packages.yaml Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,7 @@
 require("core.autocmds")
 require("core.options")
 require("config.lazy")
+
+if vim.g.neovide then
+	require("core.neovide")
+end
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -4,12 +4,17 @@ return {
 	lazy = true,
 	dependencies = {
 		'nvim-lua/plenary.nvim',
+		"nvim-telescope/telescope-project.nvim"
 	},
 	opts = {}, -- define config options here, or use a config() function
+	config = function()
+		require('telescope').load_extension('projects')
+	end,
 	keys = {
-		{ '<leader>ff', function() require 'telescope.builtin'.find_files() end, mode = 'n' },
-		{ '<leader>fg', function() require('telescope.builtin').live_grep() end, mode = 'n' },
-		{ '<leader>fb', function() require('telescope.builtin').buffers() end,   mode = 'n' },
-		{ '<leader>fh', function() require('telescope.builtin').help_tags() end, mode = 'n' }
+		{ '<leader>ff', function() require 'telescope.builtin'.find_files() end,             mode = 'n' },
+		{ '<leader>fg', function() require('telescope.builtin').live_grep() end,             mode = 'n' },
+		{ '<leader>fb', function() require('telescope.builtin').buffers() end,               mode = 'n' },
+		{ '<leader>fh', function() require('telescope.builtin').help_tags() end,             mode = 'n' },
+		{ '<leader>fp', function() require('telescope').extensions.projects.projects {} end, mode = 'n' }
 	},
 }
