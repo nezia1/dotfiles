0c75bc624c727078d50499e9f0008cfb322c4fe1
Anthony Rodriguez <anthony@nezia.dev>
2024-08-14 20:05:47 +0200

    Update .config/nvim/lua/plugins/alpha.lua

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -1,5 +1,8 @@
 return {
 	'goolord/alpha-nvim',
+	dependencies = {
+		"nvim-telescope/telescope.nvim"
+	},
 	config = function()
 		local alpha = require 'alpha'
 		local dashboard = require 'alpha.themes.dashboard'
@@ -31,6 +34,11 @@ return {
 
 		dashboard.section.header.opts.hl = 'DashboardHeader'
 
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
+			dashboard.button("p", "  Open project", ":Telescope projects<CR>"),
+		}
 		alpha.setup(dashboard.opts)
 	end
 };
