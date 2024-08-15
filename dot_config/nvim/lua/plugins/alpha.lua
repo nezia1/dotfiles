0fa1ad19a8ebdc1b811e7b14090b1c30332e359f
Anthony Rodriguez <anthony@nezia.dev>
2024-08-15 17:24:03 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/alpha.lua Update .config/nvim/lua/plugins/statusline.lua Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -1,15 +1,12 @@
 return {
 	'goolord/alpha-nvim',
-	dependencies = {
-		"nvim-telescope/telescope.nvim"
-	},
 	config = function()
 		local alpha = require 'alpha'
 		local dashboard = require 'alpha.themes.dashboard'
 
 		local frappe = require("catppuccin.palettes").get_palette "frappe"
 
-		vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = frappe.lavender })
+		vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = frappe.pink })
 
 		dashboard.section.header.val = {
 			"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⡀⠒⠒⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀",
@@ -34,7 +31,8 @@ return {
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 			dashboard.button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
-			dashboard.button("p", "  Open project", ":Telescope projects<CR>"),
+			dashboard.button("p", "  Open project",
+				":lua require'telescope'.extensions.project.project{}<CR>"),
 		}
 		alpha.setup(dashboard.opts)
 	end
