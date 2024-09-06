23542ce3fdbf702ec3ec3a69207879caf00b08e1
Anthony Rodriguez <anthony@nezia.dev>
2024-09-06 22:08:35 +0200

    add uni sync

--- hosts/solaire/default.nix
+++ hosts/solaire/default.nix
@@ -8,6 +8,33 @@
   modules.nvidia.enable = true;
   modules.gaming.enable = true;
 
+  hardware.uni-sync = {
+    enable = true;
+    devices = [
+      {
+        device_id = "VID:3314/PID:41218/SN:6243168001";
+        sync_rgb = true;
+        channels = [
+          {
+            mode = "Manual";
+            speed = 60;
+          }
+          {
+            mode = "Manual";
+            speed = 60;
+          }
+          {
+            mode = "Manual";
+            speed = 60;
+          }
+          {
+            mode = "Manual";
+            speed = 60;
+          }
+        ];
+      }
+    ];
+  };
   stylix.image = ../../wallpapers/lucy-edgerunners-wallpaper.jpg;
 }
 
