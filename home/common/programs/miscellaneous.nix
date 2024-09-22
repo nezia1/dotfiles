77159369f56330647e27367ca111109f29c1ed90
Anthony Rodriguez <anthony@nezia.dev>
2024-09-22 12:36:35 +0200

    misc: add stremio

--- home/common/programs/miscellaneous.nix
+++ home/common/programs/miscellaneous.nix
@@ -3,5 +3,5 @@
 {
   programs.fzf.enable = true;
   programs.fastfetch.enable = true;
-  home.packages = with pkgs; [ element-desktop imhex obsidian proton-pass spotify vesktop wl-clipboard ];
+  home.packages = with pkgs; [ imhex obsidian proton-pass spotify vesktop wl-clipboard stremio ];
 }
