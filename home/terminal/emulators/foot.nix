ef8d1cba3f44ff4610dbf61f187f60cf13f6410d
Anthony Rodriguez <anthony@nezia.dev>
2024-10-06 23:20:24 +0200

    home/terminal/emulators/foot: set nushell as default shell

--- home/terminal/emulators/foot.nix
+++ home/terminal/emulators/foot.nix
@@ -1,6 +1,8 @@
 {
   inputs,
   nixosConfig,
+  config,
+  lib,
   ...
 }: {
   programs.foot = {
@@ -9,6 +11,7 @@
       main = {
         term = "xterm-256color";
         font = "monospace:size=14";
+        shell = "${lib.getExe config.programs.nushell.package}";
       };
       colors = let
         schemeData = inputs.basix.schemeData.base16.${nixosConfig.style.scheme};
