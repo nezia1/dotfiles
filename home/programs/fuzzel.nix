c0c6a9b35282d1efbdf1dc4603b629a74a4fdd42
Anthony Rodriguez <anthony@nezia.dev>
2024-10-06 12:12:26 +0200

    home/programs/fuzzel: theme with base16

--- home/programs/fuzzel.nix
+++ home/programs/fuzzel.nix
@@ -1,4 +1,6 @@
 {
+  inputs,
+  nixosConfig,
   lib,
   pkgs,
   ...
@@ -8,7 +10,22 @@
     settings = {
       main = {
         terminal = lib.getExe pkgs.foot;
+        use-bold = true;
+        dpi-aware = "auto";
+        font = "monospace:size=14";
       };
+      colors = let
+        inherit (inputs.basix.schemeData.base16.${nixosConfig.theme.scheme}) palette;
+      in {
+        background = "${palette.base00}ff";
+        text = "${palette.base05}ff";
+        input = "${palette.base05}ff";
+        selection = "${palette.base02}ff";
+        selection-text = "${palette.base07}ff";
+        selection-match = "${palette.base0D}ff";
+        border = "${palette.base0E}ff";
+      };
+      border.width = 5;
     };
   };
 }
