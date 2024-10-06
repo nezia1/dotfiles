ceeb9d14ab90fffb4ce6fa850a1426760ed5f324
Anthony Rodriguez <anthony@nezia.dev>
2024-10-06 12:14:52 +0200

    profiles/vamos: import swaylock

--- home/profiles/vamos/default.nix
+++ home/profiles/vamos/default.nix
@@ -8,6 +8,7 @@
     "${self}/home/programs/ags"
     "${self}/home/programs/fuzzel.nix"
     "${self}/home/programs/swaybg.nix"
+    "${self}/home/programs/swaylock.nix"
 
     "${self}/home/terminal/emulators/foot.nix"
 
