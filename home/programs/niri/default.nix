25993d2754a0654fc06e8b0e1ec84af5f3d5b8e4
Anthony Rodriguez <anthony@nezia.dev>
2024-10-08 13:54:32 +0200

    home/programs/niri: skip hotkeys overlay at startup

--- home/programs/niri/default.nix
+++ home/programs/niri/default.nix
@@ -10,49 +10,49 @@ in {
   imports = [./binds.nix];
   programs.niri = {
     settings = {
-      environment = {
-        "NIXOS_OZONE_WL" = "1";
-        "DISPLAY" = ":0";
-        "_JAVA_AWT_WM_NONREPARENTING" = "1"; # https://wiki.archlinux.org/title/Sway#Java_applications
-      };
-
-      prefer-no-csd = true;
-
-      window-rules = [
-        {
-          draw-border-with-background = false;
-          geometry-corner-radius = let
-            r = 8.0;
-          in {
-            top-left = r;
-            top-right = r;
-            bottom-left = r;
-            bottom-right = r;
-          };
-          clip-to-geometry = true;
-        }
-        {
-          matches = [
-            {app-id = "foot";}
-          ];
-          default-column-width = {proportion = 0.5;};
-        }
-      ];
-
       input.keyboard.xkb = {
         layout = "us";
         options = "compose:ralt";
       };
+
+      prefer-no-csd = true;
+      hotkey-overlay.skip-at-startup = true;
 
       layout = {
         focus-ring = {
           enable = true;
           active.color = "#${colors.base0E}";
         };
-
         center-focused-column = "always";
         always-center-single-column = true;
       };
+    };
+
+    window-rules = [
+      {
+        draw-border-with-background = false;
+        geometry-corner-radius = let
+          r = 8.0;
+        in {
+          top-left = r;
+          top-right = r;
+          bottom-left = r;
+          bottom-right = r;
+        };
+        clip-to-geometry = true;
+      }
+      {
+        matches = [
+          {app-id = "foot";}
+        ];
+        default-column-width = {proportion = 0.5;};
+      }
+    ];
+
+    environment = {
+      "NIXOS_OZONE_WL" = "1";
+      "DISPLAY" = ":0";
+      "_JAVA_AWT_WM_NONREPARENTING" = "1"; # https://wiki.archlinux.org/title/Sway#Java_applications
     };
   };
 
