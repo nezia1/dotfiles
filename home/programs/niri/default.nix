368135be31359aea142225f6cf810c8abcec4d4d
Anthony Rodriguez <anthony@nezia.dev>
2024-10-08 15:50:08 +0200

    home/programs/niri: fix settings not under correct attribute

--- home/programs/niri/default.nix
+++ home/programs/niri/default.nix
@@ -26,33 +26,33 @@ in {
         center-focused-column = "always";
         always-center-single-column = true;
       };
-    };
 
-    window-rules = [
-      {
-        draw-border-with-background = false;
-        geometry-corner-radius = let
-          r = 8.0;
-        in {
-          top-left = r;
-          top-right = r;
-          bottom-left = r;
-          bottom-right = r;
-        };
-        clip-to-geometry = true;
-      }
-      {
-        matches = [
-          {app-id = "foot";}
-        ];
-        default-column-width = {proportion = 0.5;};
-      }
-    ];
+      window-rules = [
+        {
+          draw-border-with-background = false;
+          geometry-corner-radius = let
+            r = 8.0;
+          in {
+            top-left = r;
+            top-right = r;
+            bottom-left = r;
+            bottom-right = r;
+          };
+          clip-to-geometry = true;
+        }
+        {
+          matches = [
+            {app-id = "foot";}
+          ];
+          default-column-width = {proportion = 0.5;};
+        }
+      ];
 
-    environment = {
-      "NIXOS_OZONE_WL" = "1";
-      "DISPLAY" = ":0";
-      "_JAVA_AWT_WM_NONREPARENTING" = "1"; # https://wiki.archlinux.org/title/Sway#Java_applications
+      environment = {
+        "NIXOS_OZONE_WL" = "1";
+        "DISPLAY" = ":0";
+        "_JAVA_AWT_WM_NONREPARENTING" = "1"; # https://wiki.archlinux.org/title/Sway#Java_applications
+      };
     };
   };
 
