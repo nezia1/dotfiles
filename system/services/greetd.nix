0e08d243d105dd1f381d9f205d56f21114a8d9e4
Anthony Rodriguez <anthony@nezia.dev>
2024-10-14 17:23:01 +0200

    system/services/greetd: disable fprintd

--- system/services/greetd.nix
+++ system/services/greetd.nix
@@ -18,4 +18,17 @@
       default_session = initial_session;
     };
   };
+
+  security.pam.services.greetd.text = lib.mkIf config.services.fprintd.enable ''
+    account required pam_unix.so
+
+    # disable fprintd on greetd
+    auth sufficient pam_unix.so try_first_pass likeauth
+    auth required pam_deny.so
+
+    password sufficient pam_unix.so nullok yescrypt
+
+    session required pam_env.so conffile=/etc/pam/environment readenv=0
+    session required pam_unix.so
+  '';
 }
