46ae86929928a51e6a5dcf8ad2e8efa44a40477e
Anthony Rodriguez <anthony@nezia.dev>
2024-10-14 14:35:40 +0200

    flake: add chaotic-nyx

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
     };
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
